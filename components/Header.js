1807aec663cb0566876c49ddfd0354c0b72d7353
Khushboo Vijayvargiya <khushboovjvrg018@gmail.com>
2023-12-31 23:10:31 +0530

    Add components folder with component

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div >
+             <img className="logo" src="https://www.logodesign.net/logo/slice-of-cake-with-cherry-2703ld.png" />
+            </div>
+            <div className="nav-list">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;

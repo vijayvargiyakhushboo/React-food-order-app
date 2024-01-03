4987e67ea611014070aadfe17b9593e58e4888d0
Khushboo Vijayvargiya <khushboovjvrg018@gmail.com>
2024-01-03 23:10:44 +0530

    Create restro-list.js

---
+++ mock-data/restro-list.js
@@ -0,0 +1,480 @@
+export const restraObj = 
+    [
+      {
+        "info": {
+          "id": "728301",
+          "name": "Burger King",
+          "cloudinaryImageId": "aa77cb6fce4d94f3ad4b4e7fb42782db",
+          "locality": "New Palasia",
+          "areaName": "Indore",
+          "costForTwo": "₹350 for two",
+          "cuisines": ["Burgers", "American"],
+          "avgRating": 4.1,
+          "parentId": "166",
+          "avgRatingString": "4.1",
+          "totalRatingsString": "1K+",
+          "sla": {
+            "deliveryTime": 28,
+            "lastMileTravel": 3.8,
+            "serviceability": "SERVICEABLE",
+            "slaString": "28 mins",
+            "lastMileTravelString": "3.8 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2023-12-30 02:00:00",
+            "opened": true
+          },
+          "badges": {},
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {},
+              "textBased": {},
+              "textExtendedBadges": {}
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "60% OFF",
+            "subHeader": "UPTO ₹120"
+          },
+          "loyaltyDiscoverPresentationInfo": {
+            "logoCtx": {
+              "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+            },
+            "freedelMessage": "FREE DELIVERY",
+            "badgeType": "BADGE_TYPE_ONE_LITE"
+          },
+          "orderabilityCommunication": {
+            "title": {},
+            "subTitle": {},
+            "message": {},
+            "customIcon": {}
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {},
+              "video": {}
+            }
+          },
+          "reviewsSummary": {},
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {}
+        },
+        "analytics": {
+          "context": "seo-data-8708f981-1b93-42c9-a661-d1cae1ed989c"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/burger-king-new-palasia-indore-indore-728301",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      },
+      {
+        "info": {
+          "id": "420862",
+          "name": "La Pino'z Pizza",
+          "cloudinaryImageId": "5fe4444a065b60c4fe39ef975222c12b",
+          "locality": "Veda Complex",
+          "areaName": "Bhawar Kuan",
+          "costForTwo": "₹250 for two",
+          "cuisines": ["Pizzas", "Pastas", "Italian", "Desserts"],
+          "avgRating": 4.1,
+          "veg": true,
+          "parentId": "4961",
+          "avgRatingString": "4.1",
+          "totalRatingsString": "1K+",
+          "sla": {
+            "deliveryTime": 19,
+            "lastMileTravel": 1.9,
+            "serviceability": "SERVICEABLE",
+            "slaString": "19 mins",
+            "lastMileTravelString": "1.9 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2023-12-30 01:00:00",
+            "opened": true
+          },
+          "badges": {
+            "imageBadges": [
+              {
+                "imageId": "v1695133679/badges/Pure_Veg111.png",
+                "description": "pureveg"
+              }
+            ]
+          },
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {
+                "badgeObject": [
+                  {
+                    "attributes": {
+                      "description": "pureveg",
+                      "imageId": "v1695133679/badges/Pure_Veg111.png"
+                    }
+                  }
+                ]
+              },
+              "textBased": {},
+              "textExtendedBadges": {}
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "60% OFF",
+            "subHeader": "UPTO ₹120"
+          },
+          "loyaltyDiscoverPresentationInfo": {
+            "logoCtx": {
+              "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+            },
+            "freedelMessage": "FREE DELIVERY",
+            "badgeType": "BADGE_TYPE_ONE_LITE"
+          },
+          "orderabilityCommunication": {
+            "title": {},
+            "subTitle": {},
+            "message": {},
+            "customIcon": {}
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {},
+              "video": {}
+            }
+          },
+          "reviewsSummary": {},
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {}
+        },
+        "analytics": {
+          "context": "seo-data-8708f981-1b93-42c9-a661-d1cae1ed989c"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/la-pinoz-pizza-veda-complex-bhawar-kuan-indore-420862",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      },
+      {
+        "info": {
+          "id": "78643",
+          "name": "Pizza Hut",
+          "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+          "locality": "Transport Nagar",
+          "areaName": "Bhawar Kuan",
+          "costForTwo": "₹350 for two",
+          "cuisines": ["Pizzas"],
+          "avgRating": 3.8,
+          "parentId": "721",
+          "avgRatingString": "3.8",
+          "totalRatingsString": "5K+",
+          "sla": {
+            "deliveryTime": 25,
+            "lastMileTravel": 2.4,
+            "serviceability": "SERVICEABLE",
+            "slaString": "25 mins",
+            "lastMileTravelString": "2.4 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2023-12-30 03:00:00",
+            "opened": true
+          },
+          "badges": {},
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {},
+              "textBased": {},
+              "textExtendedBadges": {}
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "60% OFF",
+            "subHeader": "UPTO ₹120"
+          },
+          "loyaltyDiscoverPresentationInfo": {
+            "logoCtx": {
+              "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+            },
+            "freedelMessage": "FREE DELIVERY",
+            "badgeType": "BADGE_TYPE_ONE_LITE"
+          },
+          "orderabilityCommunication": {
+            "title": {},
+            "subTitle": {},
+            "message": {},
+            "customIcon": {}
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {},
+              "video": {}
+            }
+          },
+          "reviewsSummary": {},
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {}
+        },
+        "analytics": {
+          "context": "seo-data-8708f981-1b93-42c9-a661-d1cae1ed989c"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/pizza-hut-transport-nagar-bhawar-kuan-indore-78643",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      },
+      {
+        "info": {
+          "id": "292119",
+          "name": "Nafees restaurant",
+          "cloudinaryImageId": "bmmsvclvyb6fuqb9hxyo",
+          "locality": "Professor Colony",
+          "areaName": "Bhawar Kuan",
+          "costForTwo": "₹400 for two",
+          "cuisines": [
+            "Mughlai",
+            "Biryani",
+            "North Indian",
+            "Chinese",
+            "Snacks"
+            
+          ],
+          "avgRating": 4.3,
+          "parentId": "10713",
+          "avgRatingString": "4.3",
+          "totalRatingsString": "10K+",
+          "sla": {
+            "deliveryTime": 21,
+            "lastMileTravel": 1.6,
+            "serviceability": "SERVICEABLE",
+            "slaString": "21 mins",
+            "lastMileTravelString": "1.6 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2023-12-29 23:59:00",
+            "opened": true
+          },
+          "badges": {},
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {},
+              "textBased": {},
+              "textExtendedBadges": {}
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "₹125 OFF",
+            "subHeader": "ABOVE ₹249",
+            "discountTag": "FLAT DEAL"
+          },
+          "loyaltyDiscoverPresentationInfo": {
+            "logoCtx": {
+              "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+            },
+            "freedelMessage": "FREE DELIVERY",
+            "badgeType": "BADGE_TYPE_ONE_LITE"
+          },
+          "orderabilityCommunication": {
+            "title": {},
+            "subTitle": {},
+            "message": {},
+            "customIcon": {}
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {},
+              "video": {}
+            }
+          },
+          "reviewsSummary": {},
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {}
+        },
+        "analytics": {
+          "context": "seo-data-8708f981-1b93-42c9-a661-d1cae1ed989c"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/nafees-restaurant-professor-colony-bhawar-kuan-indore-292119",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      },
+      {
+        "info": {
+          "id": "67970",
+          "name": "Subway",
+          "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
+          "locality": "Snehnagar",
+          "areaName": "Sapna Sangeeta",
+          "costForTwo": "₹350 for two",
+          "cuisines": [
+            "Healthy Food",
+            "Salads",
+            "Snacks",
+            "Desserts"
+          ],
+          "avgRating": 4.2,
+          "parentId": "2",
+          "avgRatingString": "4.2",
+          "totalRatingsString": "1K+",
+          "sla": {
+            "deliveryTime": 22,
+            "lastMileTravel": 1.8,
+            "serviceability": "SERVICEABLE",
+            "slaString": "22 mins",
+            "lastMileTravelString": "1.8 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2023-12-30 00:30:00",
+            "opened": true
+          },
+          "badges": {},
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {},
+              "textBased": {},
+              "textExtendedBadges": {}
+            }
+          },
+          "loyaltyDiscoverPresentationInfo": {
+            "logoCtx": {
+              "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+            },
+            "freedelMessage": "FREE DELIVERY",
+            "badgeType": "BADGE_TYPE_ONE_LITE"
+          },
+          "orderabilityCommunication": {
+            "title": {},
+            "subTitle": {},
+            "message": {},
+            "customIcon": {}
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {},
+              "video": {}
+            }
+          },
+          "reviewsSummary": {},
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {}
+        },
+        "analytics": {
+          "context": "seo-data-8708f981-1b93-42c9-a661-d1cae1ed989c"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/subway-snehnagar-sapna-sangeeta-indore-67970",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      },
+      {
+        "info": {
+          "id": "679716",
+          "name": "Wow! Momo",
+          "cloudinaryImageId": "0984acc0ed7b914206dbbcb90297becc",
+          "locality": "Narmada Chamber",
+          "areaName": "Sangeeta Road",
+          "costForTwo": "₹300 for two",
+          "cuisines": [
+            "Tibetan",
+            "Healthy Food",
+            "Asian",
+            "Chinese",
+            "Snacks"
+            
+          ],
+          "avgRating": 4.3,
+          "parentId": "1776",
+          "avgRatingString": "4.3",
+          "totalRatingsString": "100+",
+          "sla": {
+            "deliveryTime": 25,
+            "lastMileTravel": 2.3,
+            "serviceability": "SERVICEABLE",
+            "slaString": "25 mins",
+            "lastMileTravelString": "2.3 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2023-12-29 23:55:00",
+            "opened": true
+          },
+          "badges": {},
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {},
+              "textBased": {},
+              "textExtendedBadges": {}
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "60% OFF",
+            "subHeader": "UPTO ₹120"
+          },
+          "loyaltyDiscoverPresentationInfo": {
+            "logoCtx": {
+              "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+            },
+            "freedelMessage": "FREE DELIVERY",
+            "badgeType": "BADGE_TYPE_ONE_LITE"
+          },
+          "orderabilityCommunication": {
+            "title": {},
+            "subTitle": {},
+            "message": {},
+            "customIcon": {}
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {},
+              "video": {}
+            }
+          },
+          "reviewsSummary": {},
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {}
+        },
+        "analytics": {
+          "context": "seo-data-8708f981-1b93-42c9-a661-d1cae1ed989c"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/wow-momo-narmada-chamber-sangeeta-road-indore-679716",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      }
+    ];

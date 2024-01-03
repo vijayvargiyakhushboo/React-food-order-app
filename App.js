7e1abd6050685f2163541b3f9e784ec57687ff46
Khushboo Vijayvargiya <khushboovjvrg018@gmail.com>
2024-01-03 23:09:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './App.css';
+import Header from "./components/Header";
+import Body from "./components/Body";
 
 
 
-const Header = () => {
+
+const Container = () => {
     return (
-        <div className="header">
-            <div >
-             <img className="logo" src="https://www.logodesign.net/logo/slice-of-cake-with-cherry-2703ld.png" />
-            </div>
-            <div className="nav-list">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-            </ul>
-            </div>
+        <div>
+            <Header/>
+            <Body/>
         </div>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header/>);
+root.render(<Container/>);
